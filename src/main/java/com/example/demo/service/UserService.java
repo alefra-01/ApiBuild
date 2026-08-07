b53b233fcf92c25a8a848c9bfbf8e3e4f2c47185
alefra-01 <alejandrofragon01@gmail.com>
2026-08-08 01:09:23 +0200

    hash methods implemented

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -3,14 +3,20 @@ package com.example.demo.service;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
     /*
     public UserService (UserRepository userRepository) {
@@ -21,4 +27,16 @@ public class UserService {
         return userRepository.findAll();
     }
 
+    public User createuser(User user) {
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        return userRepository.save(user);
+    }
+
+    public boolean checkPassword (User user) {
+        String username = user.getUsername();
+        Optional<User> encontrado = userRepository.findByUsername(username);
+        User usuarioFind = encontrado.get();
+        return passwordEncoder.matches(user.getPassword(), usuarioFind.getPassword());
+    }
 }
